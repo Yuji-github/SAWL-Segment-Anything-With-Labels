95893a88d3197ac10713913a9e5c8c3c40e93041
Yuji <81yishikawa@gmail.com>
2023-06-23 15:18:33 +1000

    [5] test

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# from segment_anything import SamPredictor, sam_model_registry
-
-# sam = sam_model_registry["vit_b"](checkpoint="model_checkpoint/sam_vit_b_01ec64.pth")
-# predictor = SamPredictor(sam)
-# predictor.set_image("images/dog.jpeg")
-# masks, _, _ = predictor.predict()
-
-
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 import cv2
 import torch
 import json
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -41,17 +34,38 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
+    img[:, :, 3] = 0
+    for ann in sorted_anns:
+        m = ann["segmentation"]
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+
 image = cv2.imread("images/dog.jpeg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-device = "cuda"
-sam = sam_model_registry["vit_b"](checkpoint="model_checkpoint/sam_vit_b_01ec64.pth")
+sam = sam_model_registry["vit_h"](checkpoint="model_checkpoint/sam_vit_h_4b8939.pth")
 sam.to("cuda" if torch.cuda.is_available() else "cpu")
 mask_generator = SamAutomaticMaskGenerator(sam)
 
 masks = mask_generator.generate(image)
-print(masks[0])
-with open("t.json", "w") as t:
-    json.dump(masks, t, cls=NumpyEncoder)
+# print(masks[0])
+plt.figure(figsize=(20, 20))
+plt.imshow(image)
+show_anns(masks)
+plt.axis("off")
+plt.show()
+
+# with open("t.json", "w") as t:
+#     json.dump(masks, t, cls=NumpyEncoder)
 
 
 # for i, (mask, score) in enumerate(zip(masks, scores)):
