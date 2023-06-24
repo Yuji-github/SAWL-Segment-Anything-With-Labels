72c7b76519044232a228d3a19d76f90f92e3f4cf
Yuji <81yishikawa@gmail.com>
2023-06-24 16:52:15 +1000

    [12] added comments

--- main.py
+++ main.py
@@ -126,19 +126,27 @@ def _set_up_SAM_predict_with_prompt(sam: sam_model_registry = None) -> SamPredic
     return SamPredictor(sam)
 
 
-def _show_anns(anns):
-    if len(anns) == 0:
+def _mapping_masks(masks: dict):
+    """Mapping colors with masks for each object
+
+    :param masks:
+    :return:
+    """
+    if len(masks) == 0:
         return
-    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
+    # sorting masks with area sizes (big -> small)
+    sorted_area_masks = sorted(masks, key=(lambda x: x["area"]), reverse=True)
+
     ax = plt.gca()
     ax.set_autoscale_on(False)
 
-    img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
-    img[:, :, 3] = 0
-    for ann in sorted_anns:
-        m = ann["segmentation"]
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
+    # create ones array [1, 1, 1, 1] with image height and width
+    img = np.ones((sorted_area_masks[0]["segmentation"].shape[0], sorted_area_masks[0]["segmentation"].shape[1], 4))
+    img[:, :, 3] = 0  # change the last pos to 0 [1, 1, 1, 0]
+    for mask in sorted_area_masks:
+        m = mask["segmentation"]  # get mapping masks
+        color_mask = np.concatenate([np.random.random(3), [0.35]])  # [rand, rand, rand, 0.35]
+        img[m] = color_mask  # boolean array index to give the color for each object (True areas only)
     ax.imshow(img)
 
 
@@ -177,8 +185,10 @@ if __name__ == "__main__":
     #         pass
     #         # masks = masks, _, _ = sam.predict(prompt)
 
-    # plt.figure(figsize=(20, 20))
-    # plt.imshow(image)
-    # _show_anns(masks)
-    # plt.axis("off")
-    # plt.show()
+    image = imported_images[0]
+    masks = sam.generate(image)
+    plt.figure(figsize=(20, 20))
+    plt.imshow(image)
+    _mapping_masks(masks)
+    plt.axis("off")
+    plt.show()
